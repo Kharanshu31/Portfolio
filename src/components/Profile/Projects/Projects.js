12467ece71f8ae58e759efdd3e6074b6496c7f29
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-08-12 21:04:58 +0530

    Improve Projects Block

--- src/components/Profile/Projects/Projects.js
+++ src/components/Profile/Projects/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Paper } from "@material-ui/core";
 // import {Link} from "react-router-dom";
 import "./Projects.css";
 import burger from "../../../assets/burgerbuilder.JPG";
@@ -8,13 +9,28 @@ function Projects() {
     <div className="projectdiv">
       <h1 className="projectheading">Projects</h1>
 
-      <div className="row">
+      <div className="ProjectResponsive">
 
-        <div className="container projectbox">
+
+
+        <div className="projectbox">
           <img src={burger} className="projectimg"/>
           <a href="https://react-my-burger-52295.web.app/" className="projectlink"><span>Burger Builder</span></a>
           <p className="projectinfo">Burger Builder is an online burger ordering web app built using React and Firebase</p>
         </div>
+
+        <div className="projectbox">
+          <img src={burger} className="projectimg"/>
+          <a href="https://react-my-burger-52295.web.app/" className="projectlink"><span>Burger Builder</span></a>
+          <p className="projectinfo">Burger Builder is an online burger ordering web app built using React and Firebase</p>
+        </div>
+
+        <div className="projectbox">
+          <img src={burger} className="projectimg"/>
+          <a href="https://react-my-burger-52295.web.app/" className="projectlink"><span>Burger Builder</span></a>
+          <p className="projectinfo">Burger Builder is an online burger ordering web app built using React and Firebase</p>
+        </div>
+
 
       </div>
 
