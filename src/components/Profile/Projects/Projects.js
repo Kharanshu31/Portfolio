e46479ebe26bc540a7ccd48648a8a088792b0475
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-11-14 01:58:12 +0530

    Changes in Project

--- src/components/Profile/Projects/Projects.js
+++ src/components/Profile/Projects/Projects.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Paper } from "@material-ui/core";
+// import { Paper } from "@material-ui/core";
 // import {Link} from "react-router-dom";
 import "./Projects.css";
-import burger from "../../../assets/burgerbuilder.JPG";
+import burger1 from "../../../assets/burgerbuilder.JPG";
+import burger2 from "../../../assets/burger2.JPG";
+import edumonk1 from "../../../assets/edumonk1.JPG";
+import edumonk2 from "../../../assets/edumonk2.JPG";
+import elocal1 from "../../../assets/elocal1.JPG";
+import elocal2 from "../../../assets/elocal2.JPG";
+import medicine1 from "../../../assets/medicine1.JPG";
+import medicine2 from "../../../assets/medicine2.JPG";
 import Flip from "./flip.js";
 
 function Projects() {
@@ -13,15 +20,15 @@ function Projects() {
       <div className="ProjectResponsive">
 
         <div className="projectbox">
-          <Flip img1={burger} img2={burger}/>
+          <Flip img1={burger1} img2={burger2}/>
           <a href="https://react-my-burger-52295.web.app/" className="projectlink"><span>Burger Builder</span></a>
           <p className="projectinfo">Burger Builder is an online burger ordering web app built using React and Firebase</p>
         </div>
 
         <div className="projectbox">
-          <Flip img1={burger} img2={burger}/>
-          <a href="https://react-my-burger-52295.web.app/" className="projectlink"><span>Burger Builder</span></a>
-          <p className="projectinfo">Burger Builder is an online burger ordering web app built using React and Firebase</p>
+          <Flip img1={elocal1} img2={elocal2}/>
+          <a href="https://kharanshu31.github.io/elocalProject/#/" className="projectlink"><span>E Local</span></a>
+          <p className="projectinfo">Online Shipping Website which offers wide range of products</p>
         </div>
 
       </div>
@@ -29,15 +36,15 @@ function Projects() {
       <div className="ProjectResponsive">
 
         <div className="projectbox">
-          <Flip img1={burger} img2={burger}/>
-          <a href="https://react-my-burger-52295.web.app/" className="projectlink"><span>Burger Builder</span></a>
-          <p className="projectinfo">Burger Builder is an online burger ordering web app built using React and Firebase</p>
+          <Flip img1={medicine1} img2={medicine2}/>
+          <a href="https://lit-tundra-67776.herokuapp.com/" className="projectlink"><span>Medicine Time</span></a>
+          <p className="projectinfo">MERN Stack Website which helps you remember the time you need to take your medicines</p>
         </div>
 
         <div className="projectbox">
-          <Flip img1={burger} img2={burger}/>
-          <a href="https://react-my-burger-52295.web.app/" className="projectlink"><span>Burger Builder</span></a>
-          <p className="projectinfo">Burger Builder is an online burger ordering web app built using React and Firebase</p>
+          <Flip img1={edumonk1} img2={edumonk2}/>
+          <a href="https://kharanshu31.github.io/edumonk/#/" className="projectlink"><span>Edunomics</span></a>
+          <p className="projectinfo">Online Education Provider Website for students of all age groups</p>
         </div>
 
       </div>
