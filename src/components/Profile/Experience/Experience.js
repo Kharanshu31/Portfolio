d34d3af3a619dae1bc28b7c46a69bb5dc648223c
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-11-14 21:53:49 +0530

    Update Experience

--- src/components/Profile/Experience/Experience.js
+++ src/components/Profile/Experience/Experience.js
@@ -4,11 +4,13 @@ import './Experience.css';
 
 function Experience() {
 
-  const [showEducation,setShowEducation]=useState(false)
+  const [showEducation,setShowEducation]=useState(true)
 
   return (
     <div className="ExpEduBody">
+      <div className="mainHeading">
       <h1>Experience & Education</h1>
+      </div>
 
       <div className="ButtonDiv">
       <button className="btn btn-success Expbtn" onClick={()=>setShowEducation(false)}>Experience</button>
@@ -16,39 +18,12 @@ function Experience() {
       </div>
 
       {showEducation ? (
-        <div>
-
         <div className="ExpDiv">
-
-          <p className="Timeline2">August-2019 to Present</p>
-
-          <div className="LineBox">
-            {/*<span className="LineCircle"><span style={{fontSize: '0.1rem',visibility:'hidden'}}>o</span></span>*/}
-            <span className="LineCircle">&nbsp;</span>
-          </div>
-
-          <div style={{width: '40%',textAlign: 'center',marginLeft: -80}}>
-            <h3 className="ExpHeading">Netaji Subhash University Of Technology</h3>
-            <p className="ExpDesc">B.Tech in Electronics & Communication Engineering</p>
-          </div>
-
-        </div>
-
-        <div className="ExpDiv">
-
-          <p className="Timeline3">2014-2019</p>
-
-          <div className="LineBox" style={{marginTop: -30}}>
-            {/*<span className="LineCircle"><span style={{fontSize: '0.1rem',visibility:'hidden'}}>o</span></span>*/}
-            <span className="LineCircle2">&nbsp;</span>
-          </div>
 
           <div>
-            <h3 className="ExpHeading">Laxman Public School</h3>
-            <p className="ExpDesc">PCM</p>
+            <h5>Netaji Subhas University Of Technology</h5>
+            <p>2019-2023</p>
           </div>
-
-        </div>
 
         </div>
       ) : (
