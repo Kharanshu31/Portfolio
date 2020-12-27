3d00819bb74f864f87db4970018ccf9c2de6ccb2
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-12-27 21:11:52 +0530

    Fixes in Responsiveness and image changes

--- src/components/Profile/Experience/Experience.js
+++ src/components/Profile/Experience/Experience.js
@@ -24,7 +24,18 @@ function Experience() {
 
           <div>
             <h5>Netaji Subhas University Of Technology</h5>
-            <p>2019-2023</p>
+            <div style={{display: 'flex'}}>
+              <p>2019-2023</p>
+              <p style={{marginLeft: 10}}>Majoring in Electronics & Communication Engineering</p>
+            </div>
+          </div>
+
+          <div>
+            <h5>Laxman Public School</h5>
+            <div style={{display: 'flex'}}>
+              <p>2017-2019</p>
+              <p style={{marginLeft: 10}}>PCM in 12th with 94% in CBSE boards and 10 CGPA in class 10th</p>
+            </div>
           </div>
 
         </div>
@@ -33,8 +44,21 @@ function Experience() {
         <div className="ExpDiv">
 
         <div>
-          <h5>Chattel Technologies</h5>
-          <p>July 2020-August 2020</p>
+          <h5>Web Developer at Chattel Technologies</h5>
+          <div style={{display: 'flex'}}>
+            <p>July 2020-August 2020</p>
+            <p style={{width: '60%',marginLeft: 10}}>My work was to work on a leading recruitment website.The teck stack involved was React Js,Redux for frontend while
+            backend was implemented completely using node.js/express.js and data was stored in MongoDB</p>
+          </div>
+        </div>
+
+        <div>
+          <h5>Participant at SWOC</h5>
+          <div style={{display: 'flex'}}>
+            <p>December 2020-Present</p>
+            <p style={{width: '60%',marginLeft: 10}}>It is an open-source event where participants along with the guidance of their mentors work for 2 month on various
+            open-source projects of their preferred tech stack</p>
+          </div>
         </div>
 
         </div>
