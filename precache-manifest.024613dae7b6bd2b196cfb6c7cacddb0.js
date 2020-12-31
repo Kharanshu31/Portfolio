0b574a8fdd5e44e02d0bf1cd3ad8b48225d09ab6
Kharanshu31 <dewankharanshu@gmail.com>
2021-01-01 01:28:09 +0530

    Updates

---
+++ precache-manifest.024613dae7b6bd2b196cfb6c7cacddb0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f128bb931eadd90acc80f4f9254636b0",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "54ef75cd249f883938a7",
+    "url": "/Portfolio/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "fd374f848bf7b493cade",
+    "url": "/Portfolio/static/css/main.02556450.chunk.css"
+  },
+  {
+    "revision": "54ef75cd249f883938a7",
+    "url": "/Portfolio/static/js/2.4951197f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.4951197f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd374f848bf7b493cade",
+    "url": "/Portfolio/static/js/main.88b6141a.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "f6776907138bdf0aac9aac37959a0f55",
+    "url": "/Portfolio/static/media/burger2.f6776907.JPG"
+  },
+  {
+    "revision": "7c540c0d49305ac3f4a01a3a755c526a",
+    "url": "/Portfolio/static/media/burgerbuilder.7c540c0d.JPG"
+  },
+  {
+    "revision": "ffea165b053d6a725dbffce5ce1a99b4",
+    "url": "/Portfolio/static/media/edumonk1.ffea165b.JPG"
+  },
+  {
+    "revision": "fc3207d2e802b9f597f7f7d75bcad6b9",
+    "url": "/Portfolio/static/media/edumonk2.fc3207d2.JPG"
+  },
+  {
+    "revision": "af75b5dc32906214f0ebf0712b35ebdd",
+    "url": "/Portfolio/static/media/elocal1.af75b5dc.JPG"
+  },
+  {
+    "revision": "d8c90dda4ccb69e335f6f7414f64c258",
+    "url": "/Portfolio/static/media/elocal2.d8c90dda.JPG"
+  },
+  {
+    "revision": "3c05694a1def90bff3ab48ed1c4e5868",
+    "url": "/Portfolio/static/media/medicine1.3c05694a.JPG"
+  },
+  {
+    "revision": "8c4ffc8445bb86d107e187895c8182da",
+    "url": "/Portfolio/static/media/medicine2.8c4ffc84.JPG"
+  },
+  {
+    "revision": "d078968bc905921e539a6f954e1e2ca1",
+    "url": "/Portfolio/static/media/portfolio.d078968b.jpg"
+  },
+  {
+    "revision": "61ca81f31d531f7d51a1f09da049b4d5",
+    "url": "/Portfolio/static/media/recommend1.61ca81f3.jfif"
+  }
+]);
