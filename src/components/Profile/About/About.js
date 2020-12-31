01b4b37e11011c04146cd0165e90783e4df63bb2
Kharanshu31 <dewankharanshu@gmail.com>
2021-01-01 02:07:18 +0530

    Update about section

--- src/components/Profile/About/About.js
+++ src/components/Profile/About/About.js
@@ -10,8 +10,8 @@ function About() {
       <p className="aboutpara">
         I am a Competitive Programmer and a MERN Stack Web Developer based in New Delhi.
         I am proficient in Data Structures and Algorithms and as far as Development is concerned
-        , I work on HTML, CSS, Javascript, ReactJs, Node.js, Express.js and do database management
-        through MongoDB & MySql.
+        , I work on HTML, CSS, Javascript, ReactJs, Node.js, Express.js, Mongo DB.<br />
+        <span style={{fontWeight: '900',fontSize: "1.5rem"}}>I Love To Build For Web.</span>
       </p>
     </div>
     </div>
